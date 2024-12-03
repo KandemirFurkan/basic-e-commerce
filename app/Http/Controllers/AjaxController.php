ef3fe12fe7f856a829549afaaa733f0d58b97f08
KandemirFurkan <furkankandemirr@gmail.com>
2024-12-03 18:46:47 +0300

    Send Mail

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 use App\Models\Contact;
+use App\Mail\ContactMail;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\ContentFormRequest;
 
 
@@ -19,14 +21,14 @@ $data['ip'] = request()->ip();
 
 $newdata=[
 'name'=>Str::title($request->name),
-'email'=>$request->name,
+'email'=>$request->email,
 'subject'=>$request->subject,
 'message'=>$request->message,
 'ip'=>request()->ip(),
 ];
 
 $sonkaydedilen= Contact::create($data);
-
+Mail::to('demo@gmail.com')->send(new ContactMail($newdata));
 return response()->json(['error'=>false,'message'=>'Başarıyla Gönderildi']);
 
     }
